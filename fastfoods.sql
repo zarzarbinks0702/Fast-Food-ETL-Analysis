e15afcb352705d3087d0a05d9fefdecc3756fd84
Vickie Hughes <vickie_hughes@hotmail.com>
2021-05-26 21:14:41 -0500

    upload

---
+++ fastfoods.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE fastfood (
+	FFID Text PRIMARY KEY,
+	Date Added date,
+	Business Name Text,
+	Address Text,
+	city text,
+	State text,
+	Zip INT,
+	Category text
+);
+
+CREATE TABLE population (
+	id INT PRIMARY KEY,
+	population INT,
+	mininum_age INT,
+	maximum_age INT,
+	gender Text,
+	Zipcode INT,
+	geo_id Text
+);
+
+Select * from Fastfood
+
+Select * from Population
+
